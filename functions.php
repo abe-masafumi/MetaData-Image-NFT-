40d06b55745d599375947cd962f52e81122f0eb6
masafumi <masa455.wv@gmail.com>
2021-09-30 01:29:17 +0900

    fix

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 1);
 // 「dbname」「port」「host」「username」「password」を設定
 // DB接続情報　　　　↓ここだけ自分のデータベース名に変更！
-define('DSN', 'mysql:host=mysql-2.mc.lolipop.lan;dbname=65f980f79e93e785f8038903aa8265ae');
+define('DSN', 'mysql:host=mysql-2.mc.lolipop.lan;port=3306; dbname=65f980f79e93e785f8038903aa8265ae');
 define('DB_USER', '65f980f79e93e785f8038903aa8265ae');
 define('DB_PASS', 'Masafumi455');
 
