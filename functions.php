d3c876037980b197f9e15ae269586b2204787dba
masafumi <masa455.wv@gmail.com>
2021-09-30 01:24:10 +0900

    fix

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@
 ini_set('display_errors', 1);
 // 「dbname」「port」「host」「username」「password」を設定
 // DB接続情報　　　　↓ここだけ自分のデータベース名に変更！
-define('DSN', 'mysql:host=localhost;dbname=metadataTable-NFT');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DSN', 'mysql:host=mysql-2.mc.lolipop.lan;dbname=65f980f79e93e785f8038903aa8265ae');
+define('DB_USER', '65f980f79e93e785f8038903aa8265ae');
+define('DB_PASS', 'Masafumi455');
 
 try {
   $pdo = new PDO(DSN, DB_USER, DB_PASS);
