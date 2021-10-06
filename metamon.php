8633e2391dbfa15d34c34dc2e623b9abc4f2eeed
masafumi <masa455.wv@gmail.com>
2021-10-06 20:10:21 +0900

    readmeno

--- metamon.php
+++ metamon.php
@@ -1,11 +1,10 @@
 <?php
+// metaディレクトリ内のファイル名を全て取得
 $result = glob('meta/*');
-// var_dump($result);
 $ary = [];
 foreach ($result as $value) {
-  // echo json_encode($value);
+  // ファイルの内容を全て取得して配列に入れる
   $content = json_decode(file_get_contents($value));
-
    array_push($ary, $content);
 }
 echo json_encode($ary);
