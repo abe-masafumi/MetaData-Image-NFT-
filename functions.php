ed5d6eca4c7a3a6d3e4152da64bc7b6763669cde
masafumi <masa455.wv@gmail.com>
2021-10-06 16:38:48 +0900

    fix

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 // DB接続情報　　　　↓ここだけ自分のデータベース名に変更！
 define('DSN', 'mysql:host=mysql-2.mc.lolipop.lan;port=3306; dbname=665345266565417a2349663d4afc12c1');
 define('DB_USER', '665345266565417a2349663d4afc12c1');
-define('DB_PASS', 'Masafumi455');
+define('DB_PASS', 'Masafumi4555');
 
 // ローカル用
 // define('DSN', 'mysql:host=localhost;dbname=metadataTable-NFT');
